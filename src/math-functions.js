02d7d27e9b2d9a2320b86995768c081ffeb7dd03
EmilyDSarani <emily.sarani@gmail.com>
2021-08-19 15:36:32 -0500

    first function maybe done?

--- src/math-functions.js
+++ src/math-functions.js
@@ -11,8 +11,13 @@ example and uses the values that were input into the function:
 "The sum of 4 and 7 is 11."
 */
 
-export function sum(a, b) {
-
+export function sum(a, b){
+    let sumArray = [];
+    let numOne = a;
+    let numTwo = b;
+    let total = a + b;
+    sumArray.push(`The sum of ${numOne} and ${numTwo} is ${total}.`);
+    return sumArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
